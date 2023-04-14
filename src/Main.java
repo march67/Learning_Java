e265a60279e8363a341d5fe25b7fc0db433ea4dd
march67 <td.ta.david@gmail.com>
2023-04-14 21:46:22 +0200

    Affichage d'une phrase avec la combination de "println()" et de l'échappement "\n" qui permet deux sauts de ligne mais aussi une tabulation à l'aide de "\t" dans le string de la méthode.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("J'aime le café");
         System.out.print("Surtout avec du sucre\n");
-        System.out.print("C'est encore mieux avec du lait !");
+        System.out.println("C'est encore mieux avec du lait !\n");
+        System.out.println("\t2 carrés de sucre et 20ml de lait");
     }
 }
