84a9d79d4af876e183bdd46339308fb9d609f81c
march67 <td.ta.david@gmail.com>
2023-04-15 10:24:19 +0200

    Utilisation des méthodes nextInt() et nextLine() de Scanner

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Affiche une demande à l'utilisateur
-        System.out.println("Saisie de l'utilisateur : ");
+        System.out.println("Saisie de l'utilisateur nom  : ");
         String name = scanner.nextLine(); // la console est en pause en attente d'une saisie de l'utilisateur
+        // la valeur saisie est attribué à la variable "name"
 
-        System.out.println("Le nom de l'utilisateur est : " + name); // affiche la saisie de l'utilsateur
+        System.out.println("Saisie de l'utilisateur âge : ");
+        int age = scanner.nextInt(); // nextInt() pour un entier
+
+        System.out.println("Le nom de l'utilisateur est : " + name);
+        System.out.println("L'âge de l'utilisateur est : " + age);
     }
 }
