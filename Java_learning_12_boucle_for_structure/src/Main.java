e339d8d27216f38795f30cdd1ec2df733824900d
march67 <td.ta.david@gmail.com>
2023-04-15 17:04:09 +0200

    for loop fin

--- Java_learning_12_boucle_for_structure/src/Main.java
+++ Java_learning_12_boucle_for_structure/src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
+        // structure boucle for => exécute un bloc de code limité à un nombre d'itération
 
+        //for( [initialiser la variable] ; [nombre d'itérations] ;
+        // [incrémentation de la variable à chaque itération] )
+        for(int index = 0; index<=10; index++) {
+            System.out.println(index);
+        }
+
+        System.out.println("Fin !");
     }
 }
