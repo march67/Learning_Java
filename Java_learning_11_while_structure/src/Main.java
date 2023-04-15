39e566a16b5730f84ea689741c9f88487432ec45
march67 <td.ta.david@gmail.com>
2023-04-15 16:52:37 +0200

    structure while

--- Java_learning_11_while_structure/src/Main.java
+++ Java_learning_11_while_structure/src/Main.java
@@ -1,6 +1,24 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        // boucle while => exécute un bloc de code tant que la condition est vraie
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        // l'argument de while() est la condition de la boucle
+        // tant que le String "name" est null
+        while(name.isBlank()) {
+            System.out.println("Saisissez votre nom : ");
+            name = scanner.nextLine();
+
+        }
+
+        // do{
+            // ... instructions
+        // }while(name.isBlank())
+        // structure alternative qui exécute au moins une fois les instructions
+
+        System.out.println("Bonjour : " + name);
 
     }
 }
