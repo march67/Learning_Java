266337f0a8969a7bf5921b51494a6ad59c58d797
march67 <td.ta.david@gmail.com>
2023-04-15 15:22:30 +0200

    Fin du if structure learning

--- Java_learning_8_if_structure/src/Main.java
+++ Java_learning_8_if_structure/src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // structure if => exécute le code si la condition évaluée est vraie
+        int age = 13;
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if(age==75) {
+            System.out.println("tu es âgé de 75 ans");
+        }
+        else if(age>=18) {
+            System.out.println("tu es un adulte");
+        }
+        else if(age>=12) {
+            System.out.println("tu es un adolescent");
+        }
+        else{
+            System.out.println("tu es un enfant");
         }
     }
 }
