db7b9f5ec4c7c01d40512e7feddf2160a96af4ab
march67 <td.ta.david@gmail.com>
2023-04-15 16:19:05 +0200

    fin du learning opérateurs logiques

--- Java_learning_10_operateurs_logiques/src/Main.java
+++ Java_learning_10_operateurs_logiques/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        // opérateurs logiques : utilisés pour connecter
+        // deux expressions ou plus
+        //
+        // && = (AND) les deux conditions doivent être vraies
+        // || = (OR) une des deux conditions doit être vraie
+        // ! = (NOT) inverse la valeur booléene de la condition
+
+        int temperature = 25;
+
+        // utilisation de && (AND)
+        if(temperature>30) {
+            System.out.println("il fait chaud");
+        }
+        else if(temperature>=20 && temperature<=30) {
+            System.out.println("il fait bon");
+        }
+        else {
+            System.out.println("il fait froid");
+        }
+
+        // utilisation de || (OR)
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vous jouez à un jeu ! Appuyer sur q ou z pour" +
+                " quitter");
+        String reponse = scanner.next();
+        if(reponse.equals("q") || reponse.equals("z")) {
+            System.out.println("Vous avez quitter le jeu !");
+        }
+        else {
+            System.out.println("Vous êtes toujours en train de jouer !");
+        }
+        // avec la négation et && (AND)
+        // if(!reponse.equals("c") && !reponse.equals("C") ) {
+        // System.out.println("Vous avez quitter le jeu !");
+        //}
+
 
     }
 }
