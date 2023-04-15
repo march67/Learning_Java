8521aa1eae89c166a68efe59eb45d780fcd7ee3e
march67 <td.ta.david@gmail.com>
2023-04-15 11:29:08 +0200

    expressionsJava

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,29 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // expression = opérandes et opérateurs
+        // opérandes = valeurs, variables, nombres, quantité
+        // opérateurs = [ + ; - ; * ; / ; % ]
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int nombreAmis = 10;
+
+        nombreAmis = nombreAmis / 3;
+
+        System.out.println(nombreAmis);
+
+        nombreAmis = nombreAmis % 3; // modulo 3, affiche le reste
+        // ici 2
+
+        System.out.println(nombreAmis);
+
+        // opérateur d'incrémentation et de décrémentation
+        nombreAmis++;
+        nombreAmis--;
+
+        nombreAmis = 10;
+        nombreAmis = nombreAmis / 3; // résultat = 3, le programme
+        // va tronquer pour obtenir l'entier
+
     }
 }
