8d7c16491a1bfc36ea29c33bf0b2882ad0860d4b
march67 <td.ta.david@gmail.com>
2023-04-16 00:06:53 +0200

    fin for-each loop learning

--- Java_learning_20_for-each_boucle/src/Main.java
+++ Java_learning_20_for-each_boucle/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        // for-each => technique transverse pour itinérer par tous les éléments du tableau / collection
+        // avantages : moins d'étapes, plus facilement lisible
+        // désavantage : moins flexible
 
+        // initialisation d'un tableau / array
+        String[] animaux = {"chat", "chien", "rat", "oiseau"};
+
+        // début de la boucle for-each
+        // pour chaque index du tableau animaux
+        // exécute le bloc d'instruction
+        // utilise la variable i pour représenter l'index
+        for(String i : animaux) {
+            System.out.println(i);
+        }
+
+        // initialisation d'une liste dynamique, collection
+        ArrayList<String> animaux2 = new ArrayList<String>();
+
+        animaux2.add("chien");
+        animaux2.add("chat");
+        animaux2.add("rat");
+        animaux2.add("oiseau");
+
+        for(String i : animaux2) {
+            System.out.println(i);
+        }
+        // même résultat
+        //
     }
 }
