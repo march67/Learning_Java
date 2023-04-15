895207f5b6cb9b6a64bdf72c6f3ccaf8aecaa998
march67 <td.ta.david@gmail.com>
2023-04-15 12:44:23 +0200

    New : Introduction à GUI (graphical user interface)

--- Java_learning_5_GUI_Intro/src/Main.java
+++ Java_learning_5_GUI_Intro/src/Main.java
@@ -1,9 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
 
-        
+        // ouvre une boîte de dialogue
+        String nom = JOptionPane.showInputDialog("Saisissez votre nom");
+
+        // affiche dans une fenêtre le résultat de la chaîne de caractères + variable "nom"
+        JOptionPane.showMessageDialog(null, "Résultat du nom : " + nom);
+
+        // conversion de la chaîne de caractères de la saisie en integer
+        int age = Integer.parseInt(
+                JOptionPane.showInputDialog("Saisissez votre âge : "));
+
+        JOptionPane.showMessageDialog(null, "L'âge saisie est : " + age);
+
+        // taille en double
+        double taille = Double.parseDouble(
+                JOptionPane.showInputDialog("Saisissez votre taille en mètre : "));
+
+        JOptionPane.showMessageDialog(null, "La taille saisie est : " + taille + "m");
+
 
     }
 }
