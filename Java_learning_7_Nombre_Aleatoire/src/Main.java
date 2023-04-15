ba401ad47646c4d2ea044b5ffd520e8abbcb2b44
march67 <td.ta.david@gmail.com>
2023-04-15 15:05:33 +0200

    New : if structure

--- Java_learning_7_Nombre_Aleatoire/src/Main.java
+++ Java_learning_7_Nombre_Aleatoire/src/Main.java
@@ -2,6 +2,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        Random nombreAleatoire = new Random();
+
+        int y = nombreAleatoire.nextInt(6)+1; // génère un nombre aléatoire allant de 1 à 6
+        int x = nombreAleatoire.nextInt(6); // génère un nombre aléatoire allant de 0 à 5
+        System.out.println(x);
+
+        double z = nombreAleatoire.nextDouble(); // affiche une valeur entre 0 et 1 (ex : 0.6561552567485)
+        System.out.println(z);
+
+        boolean b = nombreAleatoire.nextBoolean(); // affiche "true" ou "false"
+        System.out.println(b);
 
 
     }
