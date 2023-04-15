1c686843a6a6fd2acbb23082b2c163096da48abb
march67 <td.ta.david@gmail.com>
2023-04-15 09:24:22 +0200

    Affichage de ma variable à l'intérieur d'une chaîne de caractère

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // initialisation d'une variable
+        int x = 123;
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // affichage d'une variable dans une chaîne de caractères
+        System.out.println("Voici le résultat de ma variable :" + x );
+
+
+
     }
 }
