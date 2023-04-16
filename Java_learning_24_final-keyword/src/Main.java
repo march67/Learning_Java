a98eb8dc799e0b9d25ff484e86ad068987ce1d4a
march67 <td.ta.david@gmail.com>
2023-04-16 14:26:06 +0200

    fin learning final keyword

--- Java_learning_24_final-keyword/src/Main.java
+++ Java_learning_24_final-keyword/src/Main.java
@@ -2,6 +2,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        // le mot clé final signifie que la valeur ne peut pas être modifiée
+        final double pi = 3.141559;
+
+        pi = 4; // erreur
+
+        System.out.println(pi); // erreur "Exception in thread "main" ...
+
+
 
 
     }
