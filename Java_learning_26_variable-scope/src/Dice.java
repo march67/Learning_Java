9883a1d3bcf5752d515e3dc3a7327916f52e3751
march67 <td.ta.david@gmail.com>
2023-04-16 19:09:43 +0200

    fin learning variable scope

--- Java_learning_26_variable-scope/src/Dice.java
+++ Java_learning_26_variable-scope/src/Dice.java
@@ -1,19 +1,16 @@
 import java.util.Random;
 
 public class Dice {
+    Random aleatoire; // déclare une variable de type Random
+    int nombre;
 
-    /* EN UTILISANT SEULEMENT DES VARIABLES LOCALES */
     //constructeur de Dice
     Dice() {
+        aleatoire = new Random(); // utilise une variable globale de type Random et instancie la classe Random
+        // la variable aleatoire contient désormais une référence à un objet de la classe Random
 
-        Random aleatoire = new Random(); // instancie Random
-        int nombre = 0; // initialisation d'un entier (obligatoirement initialiser sinon risque de
-        // sécurité, si seulement déclaration alors erreur et empêche la compilation)
-
-        coup(aleatoire, nombre); // appelle la méthode coup dès l'instanciation
-        // puisque les variables sont locales, on doit les transmettre par l'utilisation d'argument
+        coup(aleatoire, nombre); // utilise les deux variables globales
     }
-
     void coup(Random objet, int nombre) {
         nombre = objet.nextInt(6)+1; // nombre aléatoire de 1 à 6
         System.out.println(nombre);
