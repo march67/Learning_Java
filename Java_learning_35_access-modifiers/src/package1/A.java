2fc323358ce78418b475c51e2d8df0c0c542edfd
march67 <td.ta.david@gmail.com>
2023-04-21 07:43:49 +0200

    new : learning encapsulation

--- Java_learning_35_access-modifiers/src/package1/A.java
+++ Java_learning_35_access-modifiers/src/package1/A.java
@@ -5,14 +5,14 @@ public class A {
     // il est accessible à l'extérieur du package2
     public static void main(String[] args) {
         C c1 = new C();
-        System.out.println(c1.defautMessage); // indique que la variable n'est pas
+        // System.out.println(c1.defautMessage); // indique que la variable n'est pas
         // accessible depuis un package extérieur
 
         System.out.println(c1.publicMessage); // possible car la variable est publique
         // elle est donc accessible à l'extérieur du package2 (ici package1)
 
         B b1 = new B();
-        System.out.println(b1.privateMessage); // impossible car il s'agit d'une
+        // System.out.println(b1.privateMessage); // impossible car il s'agit d'une
         // variable privée, seulement la classe elle même peut y accéder (B)
     }
 }
