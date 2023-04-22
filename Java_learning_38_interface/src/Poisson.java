c1178b70c7d07c13a4e5d415029ea41bbd0b00a7
march67 <td.ta.david@gmail.com>
2023-04-22 20:49:01 +0200

    refactor + new learning polymorphism

--- Java_learning_38_interface/src/Poisson.java
+++ Java_learning_38_interface/src/Poisson.java
@@ -1,7 +1,6 @@
 public class Poisson implements Proie, Predator{
     // implémente plusieurs interfaces
     // implements [nom interface1, nominterface2, ...]
-
     @Override
     public void fuir() {
         System.out.println("Le poisson fuit un poisson plus grand");
