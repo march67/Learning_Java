58751ef750063369cc867b97e5defe53daa5ee14
march67 <td.ta.david@gmail.com>
2023-04-22 19:06:26 +0200

    refactor + new learning interface

--- Java_learning_38_interface/src/Main.java
+++ Java_learning_38_interface/src/Main.java
@@ -2,7 +2,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        // interface => un modèle qui peut être appliquer à une classe
+        // similaire à l'héritage, mais spécifie ce que la class doit et peut
+        // faire. Les classes peuvent appliquer une interface ou plus
+        // , l'héritage est limité à 1 super class
+
 
     }
 }
