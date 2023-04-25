22f6cae053c97512e07c1504c07ebdfd34e4f69f
march67 <td.ta.david@gmail.com>
2023-04-25 22:50:27 +0200

    fin learning pannels-GUI

--- Java_learning_47_pannels-GUI/src/Main.java
+++ Java_learning_47_pannels-GUI/src/Main.java
@@ -1,17 +1,69 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // JPanel => composant GUI (graphical user interface) qui fonctionne en tant
+        // que conteneur (container) pour contenir d'autres composants
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Icon / icône
+        ImageIcon icon = new ImageIcon("");
+        // l'icône sera ajouté au libelle "label"
+        // je décide de le laisser vide car j'ai des images trop grandes
+
+        // label / libelle
+        JLabel label = new JLabel();
+        label.setText("Bonjour"); // voir J_L_45
+        label.setIcon(icon); // voir J_L_46
+        // le libelle "label" sera ajouté à un panneau
+        // après avoir changer le layout pour le mettre par défaut
+        // .setLayout(new BorderLayout());
+        label.setVerticalAlignment(JLabel.TOP); // voir J_L_46
+        label.setHorizontalAlignment(JLabel.RIGHT); // voir J_L_46
+
+        JLabel label2 = new JLabel();
+        label2.setText("Bonjour, 2ème libelle");
+        // avec un layout manager "null"
+        // .setLayout(null);
+        label2.setBounds(100, 100, 150,100);
+
+
+        // panel / panneau
+        JPanel panelRouge = new JPanel();
+        panelRouge.setBackground(Color.red);
+        panelRouge.setBounds(0,0,250,250); // voir J_L_46
+        panelRouge.setLayout(null);
+
+        JPanel panelBleu = new JPanel();
+        panelBleu.setBackground(Color.blue);
+        panelBleu.setBounds(250,0,250,250); // voir J_L_46
+        // pour éviter l'utilisateur d'un layout manager
+        panelBleu.setLayout(null);
+        // puis utiliser la méthode setBounds()
+        panelBleu.add(label2);
+
+        JPanel panelVert = new JPanel();
+        panelVert.setBackground(Color.green);
+        panelVert.setBounds(0,250,500,250); // voir J_L_46
+        panelVert.setLayout(new BorderLayout()); // modification du layout pour
+        // utiliser le layout par défaut
+        // on peut désormais modifier proprement la position du label
+        // à voir en haut dans la section libelle !
+        panelVert.add(label); // le libelle (avec l'icône) est ajouté à l'intérieur
+
+
+        // frame / cadre
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // voir J_L_45
+        frame.setLayout(null); // voir J_L_46
+        frame.setSize(750,750); // voir J_L_46
+        frame.setVisible(true); // voir J_L_45
+        frame.add(panelRouge); // ajout du composant JPanel
+        frame.add(panelBleu); // ajout du panneau bleu
+        frame.add(panelVert); // ajout du panneau vert
+
+
+
     }
 }
